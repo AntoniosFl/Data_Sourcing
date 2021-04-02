43ba7ad4365d0e5c52f753ef6fb4a01be713feaa
Antonios Fledos <afledos@hotmail.com>
2021-04-02 20:08:51 +0200

    testing requests

--- test_py.py
+++ test_py.py
@@ -1,7 +1,8 @@
 import requests
+import ipdb
 
-r = requests.get('https://covid19.gov.gr/covid19-live-analytics/')
-
-print(r.status_code)
-
-print('Testing git pushorigin main from branch')
+url = "https://www.metaweather.com/api/location/search/?query=london"
+response = requests.get(url).json()
+ipdb.set_trace()
+city = response[0]
+print(f"{city['title']}: {city['woeid']} ({city['latt_long']})")
