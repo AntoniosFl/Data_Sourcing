2b2266dd76ac1375dc8e82ba68f0c1e73540eda7
Antonios Fledos <afledos@hotmail.com>
2021-04-05 21:42:30 +0200

    fixed bug

--- parse_mult_pages.py
+++ parse_mult_pages.py
@@ -8,6 +8,6 @@ base_url = f"https://recipes.lewagon.com/"
 
 def parse_mult_pages(ingredient, start=1):
     receipes = []
-    for i in range(start+1):
+    for i in range(max_page(ingredient)):
         receipes += parse(ingredient, start)
     return receipes
