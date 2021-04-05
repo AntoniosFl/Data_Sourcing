bb7032cb73001a8f5e68cdeaa70cf651209b6c60
Antonios Fledos <afledos@hotmail.com>
2021-04-05 21:24:50 +0200

    celaning

--- write_csv.py
+++ write_csv.py
@@ -1,7 +1,11 @@
+import sys
 import csv
+import requests
+from bs4 import BeautifulSoup
 
 
-def write_csv(ingredient, recipes):
+def write_csv(ingredient, start=1):
+    recipes = parse_mult_pages(ingredient, start)
     with open(f'{ingredient}.csv', 'w') as recipes_file:
         writer = csv.DictWriter(recipes_file, fieldnames=recipes[0].keys())
         writer.writeheader()
