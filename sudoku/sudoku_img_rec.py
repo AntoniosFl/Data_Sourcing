0b419ab263cc2c5b791efceedf2f5bcdc063d02b
Antonios Fledos <afledos@hotmail.com>
2021-04-23 20:19:52 +0200

    CNN digits recognition notebook done

--- sudoku/sudoku_img_rec.py
+++ sudoku/sudoku_img_rec.py
@@ -1,3 +1,4 @@
+from tensorflow.keras import datasets
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
@@ -5,3 +6,9 @@ from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
+
+(X_train, y_train), (X_test,
+                     y_test) = datasets.mnist.load_data(path="mnist.npz")
+
+X_train = X_train / 255
+X_test = X_test / 255
